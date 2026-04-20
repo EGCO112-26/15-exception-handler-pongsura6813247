95a75bcbd0ae39b47cc1fc994097f3e209bb343f
pongsura6813247 <pongsura.ass@student.mahidol.edu>
2026-04-20 10:18:01 +0000

    w15

--- main.cpp
+++ main.cpp
@@ -29,38 +29,42 @@ void checkDivisor(int y) {
 int main() {
     int x, y;
     double d;
-    bool retry = true;
 
-    while (retry) {
+    while (true) { 
         try {
             cout << "Enter 2 numbers: ";
-            
 
             if (!(cin >> x >> y)) throw InputFailException();
             if (abs(x) > 1000 || abs(y) > 1000) throw RangeException();
             checkDivisor(y);
 
-       
-            retry = false; 
-
+   
             d = (double)x / y;
             cout << "The result is " << d << endl;
             
+          
             for (int i = 0; i < 1; i++) {
                 cout << "Allocating memory .... " << i << endl;
+                
                 double* myarray = new double[999999999999999]; 
             }
 
+            break; 
+
         } 
         catch (const exception &e) {
+    
             cout << e.what() << endl;
 
             if (cin.fail()) {
                 cin.clear();
                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Please try again...\n" << endl;
+            } else {
+             
+                return 0;
             }
         }
     }
-
     return 0;
 }
